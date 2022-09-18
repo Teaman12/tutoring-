ebdf06254d1d019da239f30d57fbd99e6c3f6e7e
Teaman12 <smoiketoa@gmail.com>
2022-09-18 16:55:02 +0200

    second proposal

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.dir(document);
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -13,44 +14,6 @@ function close(){
     mainMenu.style.top='-100%';
 }
 
-/* add skill */
-function addItem(){
-
-    var ul = document.getElementById("dynamic-list");
-    var listskill = document.getElementById("listskill");
-    var li = document.createElement("li");
-    li.setAttribute("id",listskill.value);
-    li.appendChild(document.createTextNode(listskill.value));
-    ul.appendChild(li);  
-}
-
-function removeItem(){
-    var ul = document.getElementById("dynamic-list");
-    var listskill = document.getElementById("listskill");
-    var item = document.getElementById(listskill.value);
-    ul.removeChild(item);
-
-}
-
-/* add links */
-function addItem(){
-
-    var ul = document.getElementById("dynamic-links");
-    var listlink = document.getElementById("listlink");
-    var li = document.createElement("li");
-    li.setAttribute("id",listlink.value);
-    li.appendChild(document.createTextNode(listlink.value));
-    ul.appendChild(li);  
-}
-
-function removeItem(){
-    var ul = document.getElementById("dynamic-links");
-    var listlink = document.getElementById("listlink");
-    var item = document.getElementById(listlink.value);
-    ul.removeChild(item);
-
-}
-
 var samples = document.getElementById("scrolling-images");
 var imageArray = ["pickup1.JPG", "pickup2.jpg", "pickup3.jpg"];
 var imageIndex = 1;
@@ -61,3 +24,4 @@ function changeImage(){
     if (imageIndex>3) {imageIndex=0;}
 }
 
+console.log(document.domain);
