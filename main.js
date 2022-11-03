1937ba40d840f035f0d09944794206c97c268b85
Teaman12 <smoiketoa@gmail.com>
2022-11-03 15:27:25 +0200

    last but not last commit

--- main.js
+++ main.js
@@ -10,18 +10,9 @@ function show(){
     mainMenu.style.display='flex';
     mainMenu.style.top='0';
 }
-function close(){
-    mainMenu.style.top='-100%';
-}
-
-var samples = document.getElementById("scrolling-images");
-var imageArray = ["pickup1.JPG", "pickup2.jpg", "pickup3.jpg"];
-var imageIndex = 1;
-
-function changeImage(){
-    samples.setAttribute("src", imageArray[imageIndex]);
-    imageIndex++;
-    if (imageIndex>3) {imageIndex=0;}
+function close() {
+    mainMenu.style.display='flex';
+    mainMenu.style.top='100%';
 }
 
 console.log(document.domain);
