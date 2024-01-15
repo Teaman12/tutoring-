b97a0e39dda65ad352237ab5240e8bf0707ea919
Teaman12 <smoiketoa@gmail.com>
2024-01-15 22:56:54 +0200

    update logo

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ console.dir(document);
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+var social = document.querySelector('.icons');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
-function show(){
+function show() {
     mainMenu.style.display='flex';
-    mainMenu.style.top='0';
-}
-function close() {
-    mainMenu.style.display='flex';
-    mainMenu.style.top='100%';
+    mainMenu.style.top = '0';
+    social.style.display = 'flex';
+    closeMenu.style.display = 'inline';
 }
 
-console.log(document.domain);
+function close() {
+    mainMenu.style.display='flex';
+    mainMenu.style.top='-100%';
+}
+
